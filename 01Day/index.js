737eb05e0184be7b83728184a407be926af1cf7b
codingBySahil <muhammadsahil345737@gmail.com>
2025-06-15 22:56:23 +0500

    Implement basic routing for the Node.js server

--- 01Day/index.js
+++ 01Day/index.js
@@ -1,6 +1,15 @@
 let http = require("http");
-let server = http.createServer((req,res)=>(
-    res.end("<h1>Hello my first Node.js server! </h1>")
-));
+let server = http.createServer((req,res)=>{
+    // res.end("<h1>Hello my first Node.js server! </h1>");
+    // adding diffrent route according to node.js documentation
+    if(req.url==="/") {
+        res.end("<h1>Home Page</h1>");
+    }else if(req.url==="/about") {
+        res.end("<h1>About Page</h1>");
+    }else if(req.url==="/news") {
+        res.end("<h1>News Page</h1>");
+    }
+
+});
 
 server.listen("8000")
