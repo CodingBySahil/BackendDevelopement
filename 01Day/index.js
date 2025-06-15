4d9875cad8f6ef61274ecd599f12d7a216c5cc09
codingBySahil <muhammadsahil345737@gmail.com>
2025-06-15 23:00:56 +0500

    Add news data endpoint to server

--- 01Day/index.js
+++ 01Day/index.js
@@ -2,12 +2,32 @@ let http = require("http");
 let server = http.createServer((req,res)=>{
     // res.end("<h1>Hello my first Node.js server! </h1>");
     // adding diffrent route according to node.js documentation
+    let newsData={
+        status:200,
+        data:[
+            {
+                id:1,
+                title:"News 1",
+                description:"News 1 description"
+            },
+            {
+                id:2,
+                title:"News 2",
+                description:"News 2 description"
+            },
+            {
+                id:3,
+                title:"News 3",
+                description:"News 3 description"
+            }
+        ]
+    }
     if(req.url==="/") {
         res.end("<h1>Home Page</h1>");
     }else if(req.url==="/about") {
         res.end("<h1>About Page</h1>");
     }else if(req.url==="/news") {
-        res.end("<h1>News Page</h1>");
+        res.end(JSON.stringify(newsData));
     }
 
 });
