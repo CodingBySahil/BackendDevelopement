8b937f0399841e63375c7958cff8cfb85c418820
codingBySahil <muhammadsahil345737@gmail.com>
2025-06-17 09:35:28 +0500

    Refactor server code to use Express and improve route handling

--- 01Day/index.js
+++ 01Day/index.js
@@ -31,34 +31,54 @@
 
 // server.listen("8000");
 
+const express = require("express");
+const app = express();
 
-// start work using Express
-let express = require("express")
-let app = express();
-app.get("/",(req,res)=>{
-  res.send("Hello express as a first use")
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Root route
+app.get("/", (req, res) => {
+  res.send("Hello express as a first use");
 });
-let newsData = {
-    status: 200,
-    data: [
-      {
-        id: 1,
-        title: "News 1",
-        description: "News one description",
-      },
-      {
-        id: 2,
-        title: "News 2",
-        description: "News two description",
-      },
-      {
-        id: 3,
-        title: "News 3",
-        description: "News three description",
-      },
-    ],
-  };
-app.get("/news",(req,res)=>{
-  res.send(newsData)
+
+// News data object
+const newsData = {
+  status: 200,
+  data: [
+    { id: 1, title: "News 1", description: "News one description" },
+    { id: 2, title: "News 2", description: "News two description" },
+    { id: 3, title: "News 3", description: "News three description" },
+  ],
+};
+
+// GET: send news data
+app.get("/news", (req, res) => {
+  res.send(newsData);
 });
-app.listen("8000");
+
+// POST: receive query data 
+app.post("/querydata", (req, res) => {
+  const userData = req.query;
+  console.log("Query data:", userData);
+  res.send({ status: 201, msg: "User data from query", user: userData });
+});
+
+// POST: receive body data (JSON)
+app.post("/bodydata", (req, res) => {
+  const userData = req.body;
+  console.log("Body data:", userData);
+  res.send({ status: 201, msg: "User data from body", user: userData });
+});
+
+// POST: receive data from route param 
+app.post("/param/:id", (req, res) => {
+  const ID = req.params.id;
+  console.log("Param ID:", ID);
+  res.send({ status: 201, msg: "User data from param id", user: ID });
+});
+
+// Start server
+app.listen(8000, () => {
+  console.log("Server is running on port 8000");
+});
