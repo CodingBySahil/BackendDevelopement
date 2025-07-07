09774e4fb77fddd6946b466fcd31677a0d6f3832
codingBySahil <muhammadsahil345737@gmail.com>
2025-07-07 14:23:44 +0500

    simple initialization for frontend added

---
+++ 06DayCRUDProjectComplete/contact-book-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='min-h-screen bg-red-900 flex items-center justify-center'>
+      App
+    </div>
+  )
+}
+
+export default App
