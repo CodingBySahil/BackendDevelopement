504ad3ccb48c7b47ce358995e1e4a49dcf277296
codingBySahil <muhammadsahil345737@gmail.com>
2025-07-12 15:40:44 +0500

    feat: implement authentication controllers for user registration and login

--- 07DayAuthenticationProject/app.js
+++ 07DayAuthenticationProject/app.js
@@ -1,15 +1,13 @@
-const express = require('express');
-const { connectDB } = require('./configs/DBConnection');
-require('dotenv').config();
+const express = require("express");
+const { connectDB } = require("./configs/DBConnection");
+require("dotenv").config();
 
-connectDB()
+connectDB();
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
+app.use("/api/auth", require("./routes/authRoutes"));
 
-app.use('/api/auth', require('./routes/authRoutes'))
-
-
-app.listen(process.env.PORT || 8020,()=>{
-    console.log(`server is listning on http://localhost:${process.env.PORT}`)
-})
+app.listen(process.env.PORT || 8020, () => {
+  console.log(`server is listning on http://localhost:${process.env.PORT}`);
+});
