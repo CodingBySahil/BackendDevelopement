74d4322de1ff6848021162a78f9d29b39174108c
codingBySahil <muhammadsahil345737@gmail.com>
2025-07-31 20:57:58 +0500

    react vite with tailiwind css for frontend initialized

---
+++ 10LinkedIn/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen w-screen">
+      <h1 class="text-3xl font-bold  bg-purple-800 text-white px-6 py-3 rounded-full">
+        This will be the frontend of linkedin everthing is ready
+      </h1>
+    </div>
+  );
+};
+
+export default App;
