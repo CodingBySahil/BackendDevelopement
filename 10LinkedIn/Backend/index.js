3b4cd583ffaa363a938c40878b9ef823955396d0
codingBySahil <muhammadsahil345737@gmail.com>
2025-07-31 23:02:20 +0500

    refactor: organize middleware setup and improve code readability

--- 10LinkedIn/Backend/index.js
+++ 10LinkedIn/Backend/index.js
@@ -3,24 +3,24 @@ import dotenv from "dotenv";
 import connectDB from "./config/connectDB.js";
 import authRoutes from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8020;
 
-
-
-
-
 // middlewares
+app.use(
+  cors({
+    origin: "http://localhost:5173", // frontend port (Vite default)
+    credentials: true, // allow cookies to be sent
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/auth",authRoutes)
+app.use("/api/auth", authRoutes);
 app.use(cookieParser());
-
-
-
-
+// ✅ Must allow credentials and your frontend origin
 
 app.listen(PORT, () => {
   connectDB();
