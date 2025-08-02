68a75c0c3ff3d0338ebfa3c84feeab37c2620690
codingBySahil <muhammadsahil345737@gmail.com>
2025-08-02 18:19:11 +0500

    getallpost route added

--- 10LinkedIn/Backend/routes/post.routes.js
+++ 10LinkedIn/Backend/routes/post.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, getALlPosts } from "../controllers/post.controller.js";
 import upload from "../middlewares/multer.js";
 import isAuth from "../middlewares/isAuth.js";
 
@@ -8,4 +8,5 @@ const postRoutes = express.Router();
 
 // Route for creating a post
 postRoutes.post("/create-post", isAuth, upload.single("image"), createPost);
+postRoutes.get("/get-all-posts", isAuth, getALlPosts);
 export default postRoutes;
