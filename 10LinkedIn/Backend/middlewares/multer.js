23eb44111ba76a997250e1e81e8ae860dd41c7b4
codingBySahil <muhammadsahil345737@gmail.com>
2025-08-02 09:26:18 +0500

    prety the code

--- 10LinkedIn/Backend/middlewares/multer.js
+++ 10LinkedIn/Backend/middlewares/multer.js
@@ -1,11 +1,11 @@
 import multer from 'multer'
-const upload = multer({storage});
 let storage = multer.diskStorage({
     destination:(req,file,cb)=>{
-        cb(null,'/public')
+        cb(null,'./public')
     },
     filename:(req,file,cb)=>{
         cb(null,file.originalname)
     }
 })
+const upload = multer({storage});
 export default upload
