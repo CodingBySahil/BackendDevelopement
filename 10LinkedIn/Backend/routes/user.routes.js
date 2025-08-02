6fc0764cb48c966546616237f180020c2e9407bd
codingBySahil <muhammadsahil345737@gmail.com>
2025-08-02 09:26:43 +0500

    upload route created

--- 10LinkedIn/Backend/routes/user.routes.js
+++ 10LinkedIn/Backend/routes/user.routes.js
@@ -1,9 +1,22 @@
 import express from "express";
-import { getCurrentUser } from "../controllers/user.controllers.js";
+import {
+  getCurrentUser,
+  updateProfile,
+} from "../controllers/user.controllers.js";
 import isAuth from "../middlewares/isAuth.js";
+import upload from "../middlewares/multer.js";
 
 const userRoutes = express.Router();
 
 userRoutes.get("/get-current-user", isAuth, getCurrentUser);
+userRoutes.put(
+  "/update-profile",
+  isAuth,
+  upload.fields([
+    { name: "profileImage", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
+  ]),
+  updateProfile
+);
 
 export default userRoutes;
