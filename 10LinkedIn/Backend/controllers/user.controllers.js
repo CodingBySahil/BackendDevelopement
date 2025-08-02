e982f7b2d75e1eefcb0831d11b12e601de67254f
codingBySahil <muhammadsahil345737@gmail.com>
2025-08-02 09:25:45 +0500

    update profile cotroller added

--- 10LinkedIn/Backend/controllers/user.controllers.js
+++ 10LinkedIn/Backend/controllers/user.controllers.js
@@ -1,3 +1,4 @@
+import uploadOnCloudinary from "../config/cloudinary.js";
 import User from "../models/user.model.js";
 
 export const getCurrentUser = async (req, res) => {
@@ -14,5 +15,41 @@ export const getCurrentUser = async (req, res) => {
   } catch (error) {
     console.error("Error fetching current user:", error.message);
     return res.status(500).json({ message: "Internal server error." });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { firstName, lastName, userName, headline, location, gender } =
+      req.body;
+    const skills = req.body.skills ? JSON.parse(req.body.skills) : [];
+    const education = req.body.education ? JSON.parse(req.body.education) : [];
+    const experience = req.body.experience
+      ? JSON.parse(req.body.experience)
+      : [];
+    let profileImage, coverImage;
+    if (req.files.profileImage) {
+      profileImage = await uploadOnCloudinary(req.files.profileImage[0].path);
+    }
+    if (req.files.coverImage) {
+      coverImage = await uploadOnCloudinary(req.files.coverImage[0].path);
+    }
+    let user = await User.findByIdAndUpdate(req.userId, {
+      firstName,
+      lastName,
+      userName,
+      headline,
+      location,
+      gender,
+      skills,
+      education,
+      experience,
+      profileImage,
+      coverImage,
+    },{new:true}).select("-password");
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "error in handle update controller" });
   }
 };
