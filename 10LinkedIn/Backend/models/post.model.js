8fcfc4fdec098031c5ab460932f4f9f2bc32555d
codingBySahil <muhammadsahil345737@gmail.com>
2025-08-02 09:27:06 +0500

    post schema added

---
+++ 10LinkedIn/Backend/models/post.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      require: true,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    image: {
+      type: String,
+    },
+    like: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    comment: [
+      {
+        content: { type: String, require: true },
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+const Post = mongoose.model("Post", postSchema);
